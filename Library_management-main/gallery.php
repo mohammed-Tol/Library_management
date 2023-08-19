16071f6aa16efd91b940c5ee331e2fa721e3e500
Mohammed Tolha Baig <85863442+mohammed-Tol@users.noreply.github.com>
2023-08-19 11:53:29 +0530

    Add files via upload

---
+++ Library_management-main/gallery.php
@@ -0,0 +1,30 @@
+<?php
+include('header.php')
+?>
+  
+    <div class="row mt-4">
+    <div class="col-4 gallery">
+    <img src="images/1.jpg" class="img-fluid">
+    </div>
+    <div class="col-4 gallery">
+    <img src="images/2.jpg" class="img-fluid">
+    </div>
+    <div class="col-4 gallery">
+    <img src="images/3.jpg" class="img-fluid">
+    </div>
+    </div>
+    <div class="row my-3">
+    <div class="col-4 gallery">
+    <img src="images/4.jpg" class="img-fluid">
+    </div>
+	    <div class="col-4 gallery">
+    <img src="images/5.jpeg" class="img-fluid">
+    </div>
+	    <div class="col-4 gallery">
+    <img src="images/6.jpg" class="img-fluid">
+    </div>
+    </div>
+    </div>
+<?php
+include('footer.php')
+?>
